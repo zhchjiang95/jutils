9b082ab9d0e0427949bb82cc586e6bcc6c8ae0b8
protoo <i@fiume.cn>
2024-08-09 10:56:33 +0800

    dev: plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import babel from "@rollup/plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript";
 import copy from 'rollup-plugin-copy';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: "src/index.ts", // 打包入口
@@ -17,6 +18,7 @@ export default {
     // 打包插件
     resolve(), // 查找和打包node_modules中的第三方模块
     commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
+    terser(),
     typescript(), // 解析TypeScript
     babel({ babelHelpers: "bundled" }), // babel配置,编译es6
     copy({
