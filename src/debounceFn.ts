871f83af3ee3d7d69edeb387a646b01d9a163dce
protoo <i@fiume.cn>
2024-09-25 16:01:50 +0800

    fix: jsdoc error

--- src/debounceFn.ts
+++ src/debounceFn.ts
@@ -2,7 +2,7 @@
  * 防抖函数，在连续的函数触发过程执行最后一次
  * @param fn 连续触发的函数
  * @param ms 执行等待的毫秒数，默认 200
- * @param opt { maxWait: 最大等待时间，超过立即执行一次 }
+ * @param opt 配置对象 { maxWait: 最大等待时间，超过立即执行一次 }
  * @returns A new, debounce, function.
  * @example
  * ```js
